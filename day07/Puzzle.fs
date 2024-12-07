6cab1975914ad4181c4611b289a7e15ea14eb131
Kalle Brisland <kalle.brisland@cygni.se>
2024-12-07 10:43:32 +0100

    Day 7: replaced list literals with arrays

--- day07/Puzzle.fs
+++ day07/Puzzle.fs
@@ -7,16 +7,16 @@ let private createEquation (line: string) =
     let parts = line.Split(": ")
     { testValue = int64 parts[0]; numbers = parts[1].Split(' ') |> Array.map int64 |> Array.toList }
 
-let private canBeTrue (operations: Operation list) (equation: Equation) =
+let private canBeTrue (operations: Operation[]) (equation: Equation) =
     let rec loop (numbers: int64 list) (curValue: int64) =
         if numbers.IsEmpty then
             curValue = equation.testValue
         else
-            operations |> List.exists (fun operation -> loop numbers.Tail (operation curValue numbers.Head))
+            operations |> Array.exists (fun operation -> loop numbers.Tail (operation curValue numbers.Head))
 
     loop equation.numbers.Tail equation.numbers.Head
 
-let private calibrationResult (operations: Operation list) (equations: Equation[]) =
+let private calibrationResult (operations: Operation[]) (equations: Equation[]) =
     equations
     |> Array.filter (canBeTrue operations)
     |> Array.sumBy _.testValue
@@ -25,6 +25,6 @@ let private concat number1 number2 = int64 $"{number1}{number2}"
 
 let parseInput (rawInput: string) = rawInput.Split('\n') |> Array.map createEquation
 
-let runPartOne (input: Equation[]) = input |> calibrationResult [(+); (*)]
+let runPartOne (input: Equation[]) = input |> calibrationResult [| (+); (*) |]
 
-let runPartTwo (input: Equation[]) = input |> calibrationResult [(+); (*); concat]
+let runPartTwo (input: Equation[]) = input |> calibrationResult [| (+); (*); concat |]
